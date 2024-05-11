7faba20736d2ad73e376d88d8fdb00362209ad93
dnielpy <reynaldo.dquesada@estudiantes.matcom.uh.cu>
2024-05-11 19:16:02 -0400

    added all rutes

--- 03-Simple API/src/main/java/com/example/demo/Controller.java
+++ 03-Simple API/src/main/java/com/example/demo/Controller.java
@@ -5,7 +5,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
     @GetMapping("/json")
-    public String hola() {
+    public String json() {
         return "Aqui se devuelve el archivo JSON";
+    }
+    @GetMapping("/xml")
+    public String xml() {
+        return "Aqui se devuelve el archivo XML";
+    }
+    @GetMapping("/csv")
+    public String csv() {
+        return "Aqui se devuelve el archivo CSV";
     }
 }
