76ee17e11b82a29a76d5eac8d8978885e869d62e
dnielpy <reynaldo.dquesada@estudiantes.matcom.uh.cu>
2024-05-11 22:32:01 -0400

    connected the BD with the API

--- 03-Simple API/src/main/java/com/example/demo/Controller.java
+++ 03-Simple API/src/main/java/com/example/demo/Controller.java
@@ -1,16 +1,25 @@
 package com.example.demo;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
 
 @RestController
 public class Controller {
+    @Autowired
+    private PersonRepository personRepository;
+
     @GetMapping("/json")
     public String json() {
         return "Aqui se devuelve el archivo JSON";
     }
     @GetMapping("/xml")
-    public String xml() {
-        return "Aqui se devuelve el archivo XML";
+    public List<Data> xml() {
+        //sacar los datos de la base de datos
+        List<Data> people = personRepository.findAll();
+
+        return people;
     }
     @GetMapping("/csv")
     public String csv() {
---
+++ 03-Simple API/src/main/java/com/example/demo/Data.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Entity
+public class Data {
+    @Id
+    private int id;
+    private String name;
+    private String email;
+    private String gender;
+    private String number;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+}
