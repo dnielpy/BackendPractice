310f9c2497e7a3719723e52de61af71c76cefc3d
dnielpy <reynaldo.dquesada@estudiantes.matcom.uh.cu>
2024-05-14 14:44:40 -0400

    portafolio initial commit

---
+++ 04-Portafolio/src/test/java/com/example/demo/PortafolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortafolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
