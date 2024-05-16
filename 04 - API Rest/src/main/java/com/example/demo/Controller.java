33c1a2efd2e247afdb1a8b817ac2e1fc525cd642
dnielpy <reynaldo.dquesada@estudiantes.matcom.uh.cu>
2024-05-16 11:11:18 -0400

    Paths Added

--- 04 - API Rest/src/main/java/com/example/demo/Controller.java
+++ 04 - API Rest/src/main/java/com/example/demo/Controller.java
@@ -11,9 +11,27 @@ public class Controller {
     @Autowired
     private StudentsRepository studentsRepository;
 
-    @GetMapping("/")
-    public List<String> index(){
-        List<studentsdatabase> Estudiantes = studentsRepository.findAll();
-        return Estudiantes.stream().map(studentsdatabase::getName).collect(Collectors.toList());
+    @GetMapping("/names")
+    public List<String> getNames() {
+        List<studentsdatabase> people = studentsRepository.findAll();
+        return people.stream().map(studentsdatabase::getName).collect(Collectors.toList());
+    }
+
+    @GetMapping("/emails")
+    public List<String> getEmails() {
+        List<studentsdatabase> people = studentsRepository.findAll();
+        return people.stream().map(studentsdatabase::getEmail).collect(Collectors.toList());
+    }
+
+    @GetMapping("/genders")
+    public List<String> getGenders() {
+        List<studentsdatabase> people = studentsRepository.findAll();
+        return people.stream().map(studentsdatabase::getGender).collect(Collectors.toList());
+    }
+
+    @GetMapping("/numbers")
+    public List<String> getNumbers() {
+        List<studentsdatabase> people = studentsRepository.findAll();
+        return people.stream().map(studentsdatabase::getNumber).collect(Collectors.toList());
     }
 }
