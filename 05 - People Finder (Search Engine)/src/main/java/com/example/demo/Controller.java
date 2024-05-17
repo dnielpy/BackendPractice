6fb2b5f69c33d574afb1e8480aa8e51c7e4d6648
dnielpy <reynaldo.dquesada@estudiantes.matcom.uh.cu>
2024-05-17 13:22:51 -0400

    tengo un error pero lo va a resolver copilot

--- 05 - People Finder (Search Engine)/src/main/java/com/example/demo/Controller.java
+++ 05 - People Finder (Search Engine)/src/main/java/com/example/demo/Controller.java
@@ -1,16 +1,23 @@
 package com.example.demo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class Controller {
-    SearchEngine searchengine = new SearchEngine("hombre blanco edad 35");
+    @Autowired
+    private PeopleRepository peopleRepository;
+
+    List<PeopleDatabase> data = peopleRepository.findAll();
+
+    SearchEngine searchengine = new SearchEngine("hombre blanco edad 35", data);
 
     @GetMapping("/")
-    public List<String> getStudent() {
+    public List<PeopleDatabase> getStudent() {
         searchengine.setImportant_words("edad");
         searchengine.filter();
-        return searchengine.getQuery_information();
+        searchengine.filter_people();
+        return searchengine.getSospechosos();
     }
 }
--- 05 - People Finder (Search Engine)/src/main/java/com/example/demo/SearchEngine.java
+++ 05 - People Finder (Search Engine)/src/main/java/com/example/demo/SearchEngine.java
@@ -7,14 +7,14 @@ public class SearchEngine {
     private List<String> query_words;
     private List<String> query_information = new ArrayList<>();
     private List<String> important_words = new ArrayList<>();
-    private List<PeopleDatabase> all_people_data = new ArrayList<>();
-    private List<PeopleDatabase> filtred_people_data = new ArrayList<>();
-    private PeopleRepository peopleRepository;
+    private List<PeopleDatabase> sospechosos = new ArrayList<>();
+    private List<PeopleDatabase> persons_data;
 
     //Constructor
-    public SearchEngine(String Query){
+    public SearchEngine(String Query, List<PeopleDatabase>PersonData){
         this.query = Query;
         this.query_words = List.of(this.query.split(" "));
+        this.persons_data = PersonData;
     }
 
     //Text Treatment
@@ -23,7 +23,7 @@ public class SearchEngine {
     }
 
     //Determine if the word is a key word
-    public boolean itsAKey(String x){
+    private boolean itsAKey(String x){
         if (important_words.contains(x)) {
             return true;
         }
@@ -48,20 +48,31 @@ public class SearchEngine {
         }
     }
 
-    public List<String> getQuery_information() {
-        return query_information;
+    public void filter_people(){
+        //Usando las palabras claves, sacar las personas que coinciden
+
+
+        //Recorrer sus propieddes y si son iguales a lo que me pasaron por parametro, añadirlos a filtredp_people_data
+        for (int i = 0; i < persons_data.size(); i++) {
+            if (
+                    query_information.contains(persons_data.get(i).getName().toLowerCase())
+                    ^ query_information.contains(persons_data.get(i).getAge().toLowerCase())
+                    ^ query_information.contains(persons_data.get(i).getCountry().toLowerCase())
+                    ^ query_information.contains(persons_data.get(i).getColor().toLowerCase())
+                    ^ query_information.contains(persons_data.get(i).getGender().toLowerCase())
+                    ^ query_information.contains(persons_data.get(i).getId().toLowerCase())
+                    ^ query_information.contains(persons_data.get(i).getPhone().toLowerCase())
+            ) {
+                sospechosos.add(persons_data.get(i));
+            }
+        }
+    }
+
+    public List<PeopleDatabase> getSospechosos() {
+        return sospechosos;
     }
 
     public void setImportant_words(String important_words) {
         this.important_words.add(important_words);
-    }
-
-    public void filter_people(){
-        //Usando las palabras claves, sacar las personas que coinciden
-        //Sacar los datos de la bd
-        List<PeopleDatabase> persons_data = peopleRepository.findAll();
-
-        //Recorrer sus propieddes y si son iguales a lo que me pasaron por parametro, añadirlos a filtredp_people_data
-
     }
 }
