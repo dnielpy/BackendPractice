86b9dc749c2f6da4d4e319bd42a2dd766857125c
dnielpy <reynaldo.dquesada@estudiantes.matcom.uh.cu>
2024-05-23 18:18:29 -0400

    05 - viewProducts - Created class and repository

---
+++ 05 - API REST Inventory Management/src/main/java/com/example/demo/ProductReository.java
@@ -0,0 +1,6 @@
+package com.example.demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductReository extends JpaRepository<Products, Integer>{
+}
---
+++ 05 - API REST Inventory Management/src/main/java/com/example/demo/Products.java
@@ -0,0 +1,45 @@
+package com.example.demo;
+
+public class Products {
+    private int id;
+    private String name;
+    private int price;
+
+    public Products() {
+    }
+
+    public Products(int id, String name, int price){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Products [id=" + id + ", name=" + name + ", price=" + price + "]";
+    }
+}
