c697f4a235715b699cae946f9f6639c4cab70a4d
dnielpy <reynaldo.dquesada@estudiantes.matcom.uh.cu>
2024-05-23 18:30:07 -0400

    05 - getProduct - Done

--- 05 - API REST Inventory Management/src/main/java/com/example/demo/Controller.java
+++ 05 - API REST Inventory Management/src/main/java/com/example/demo/Controller.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,5 +16,11 @@ public class Controller {
     public List<Products> viewProducts(){
         List<Products> products = productsRepository.findAll();
         return products;
+    }
+
+    @PostMapping("/viewProducts")
+    public Products getPreoduct(@RequestParam int id){
+        Products product = productsRepository.findById(id).orElse(null);
+        return product;
     }
 }
