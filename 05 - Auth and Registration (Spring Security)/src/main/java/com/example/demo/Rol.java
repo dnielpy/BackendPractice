d4a10a6645664bc4af4f3ff2c257393675916aa4
dnielpy <reynaldo.dquesada@estudiantes.matcom.uh.cu>
2024-05-30 10:04:25 -0400

    05 - Rol - Entidad completa

--- 05 - Auth and Registration (Spring Security)/src/main/java/com/example/demo/Rol.java
+++ 05 - Auth and Registration (Spring Security)/src/main/java/com/example/demo/Rol.java
@@ -7,4 +7,29 @@ import jakarta.persistence.Id;
 public class Rol {
     @Id
     private long id;
+    private String name;
+
+    public Rol() {
+    }
+
+    public Rol(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
