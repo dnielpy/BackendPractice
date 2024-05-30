de61275ba95c0d4333eb08e9483d843fb312691d
dnielpy <reynaldo.dquesada@estudiantes.matcom.uh.cu>
2024-05-30 09:44:54 -0400

    05 - Base de datos conectada

--- 05 - Auth and Registration (Spring Security)/src/main/java/com/example/demo/UsersRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsersRepository extends JpaRepository <Users, Integer> {
-    //Este método es para buscar un usuario por su nombre de usuario
-    Users findByUsername(String username);
-}
