b1c018604c15577919b33e57a52c5a95e8c93784
dnielpy <reynaldo.dquesada@estudiantes.matcom.uh.cu>
2024-05-30 09:48:56 -0400

    05 - Users - Agregada la entidad

---
+++ 05 - Auth and Registration (Spring Security)/src/main/java/com/example/demo/Users.java
@@ -0,0 +1,65 @@
+package com.example.demo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Users {
+    @Id
+    private long id;
+    private String name;
+    private String email;
+    private String password;
+
+    public Users() {
+    }
+
+    public Users(long id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
