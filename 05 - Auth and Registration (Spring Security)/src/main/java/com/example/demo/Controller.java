a8d38d7bf92f3bdd23550c508234715545b24662
dnielpy <reynaldo.dquesada@estudiantes.matcom.uh.cu>
2024-06-04 11:51:13 -0400

    05 - UserDTO - Created

---
+++ 05 - Auth and Registration (Spring Security)/src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+}
---
+++ 05 - Auth and Registration (Spring Security)/src/main/java/com/example/demo/UserDTO.java
@@ -0,0 +1,50 @@
+package com.example.demo;
+
+public class UserDTO {
+    private long id;
+    private String username;
+    private String password;
+
+    //Constructors
+    public UserDTO(long id, String username, String password) {
+        super();
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    //Getters and Setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    //ToString
+    @Override
+    public String toString() {
+        return "Users{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
