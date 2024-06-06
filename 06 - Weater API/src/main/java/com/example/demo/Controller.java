b330d27d4ebfb65e9cdf2b7e94f7901e10ede491
dnielpy <reynaldo.dquesada@estudiantes.matcom.uh.cu>
2024-06-06 11:12:54 -0400

    06 - Ya funciona a un nivel basico, voy a agregar los parametros que se pasan desde el controllador

--- 06 - Weater API/src/main/java/com/example/demo/Controller.java
+++ 06 - Weater API/src/main/java/com/example/demo/Controller.java
@@ -1,18 +1,18 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
-@org.springframework.stereotype.Controller
+@RestController
 public class Controller {
 
     @GetMapping("/getWeather")
-    public String getWeather(){
+    public Mono<String> getWeather(){
         WebClient webClient = WebClient.create();
-        WeatherService weatherService = new WeatherService("37.7749", "-122.4194", webClient);
-        Mono<String> weather = weatherService.getWeather();
-        return weather.block();
+        WeatherService weatherService = new WeatherService(webClient);
+        return weatherService.getWeather();
     }
 }
 
--- 06 - Weater API/src/main/java/com/example/demo/WeatherService.java
+++ 06 - Weater API/src/main/java/com/example/demo/WeatherService.java
@@ -4,20 +4,14 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 public class WeatherService {
-    private String latitude;
-    private String longitude;
     private final WebClient webClient;
 
-    public WeatherService(String latitude, String longitude, WebClient webClient) {
-        this.latitude = latitude;
-        this.longitude = longitude;
+    public WeatherService(WebClient webClient) {
         this.webClient = webClient;
     }
-
-    public Mono<String> getWeather(){
-        String urlTemplate = "http://api.openweathermap.org/data/2.5/weather?lat={" + latitude + "}&lon={" + longitude + "}";
+    public Mono<String> getWeather() {
         return webClient.get()
-                .uri("http://www.google.com")
+                .uri("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m")
                 .retrieve()
                 .bodyToMono(String.class);
     }
