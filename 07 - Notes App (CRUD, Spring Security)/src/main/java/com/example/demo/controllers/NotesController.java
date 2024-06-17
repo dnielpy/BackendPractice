98948012b5a7790a1b62a927b169318c39998cf5
dnielpy <reynaldo.dquesada@estudiantes.matcom.uh.cu>
2024-06-17 19:13:50 -0400

    07 - Notes - getNote funciona

--- 07 - Notes App (CRUD, Spring Security)/src/main/java/com/example/demo/controllers/NotesController.java
+++ 07 - Notes App (CRUD, Spring Security)/src/main/java/com/example/demo/controllers/NotesController.java
@@ -21,22 +21,25 @@ public class NotesController {
     ListRepository listRepository;
 
     @PostMapping
-    public String createNote(@RequestParam String tittle, @RequestParam String note, Principal principal){
+    public String createNote(@RequestParam String tittle, @RequestParam String note, Principal principal) {
         NotesService notesService = new NotesService(principal.getName(), tittle, note, userRepository, noteRepository, listRepository);
         return notesService.createNote();
     }
+
     @GetMapping()
-    public String getNote(@RequestParam String tittle, Principal principal){
+    public String getNote(@RequestParam String tittle, Principal principal) {
         NotesService notesService = new NotesService(tittle, principal.getName(), userRepository, noteRepository, listRepository);
         return notesService.getNote();
     }
+
     @PutMapping
-    public String updateNote(@RequestParam String tittle, @RequestParam String note){
+    public String updateNote(@RequestParam String tittle, @RequestParam String note) {
         NotesService notesService = new NotesService(tittle, note, userRepository, noteRepository, listRepository);
         return notesService.updateNote();
     }
+
     @DeleteMapping
-    public String deleteNote(@RequestParam String tittle, Principal principal){
+    public String deleteNote(@RequestParam String tittle, Principal principal) {
         NotesService notesService = new NotesService(tittle, principal.getName(), userRepository, noteRepository, listRepository);
         return notesService.getNote();
     }
