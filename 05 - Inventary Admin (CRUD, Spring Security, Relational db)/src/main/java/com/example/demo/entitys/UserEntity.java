79a767efc7317a95ad4d4387cd73e121a47f0481
dnielpy <reynaldo.dquesada@estudiantes.matcom.uh.cu>
2024-06-18 14:19:35 -0400

    05 - Inventory Admin - UserEntity - Created

--- 05 - Inventary Admin (CRUD, Spring Security, Relational db)/src/main/java/com/example/demo/entitys/UserEntity.java
+++ 05 - Inventary Admin (CRUD, Spring Security, Relational db)/src/main/java/com/example/demo/entitys/UserEntity.java
@@ -23,4 +23,45 @@ public class UserEntity {
     public UserEntity() {
     }
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public long getCredit() {
+        return credit;
+    }
+
+    public void setCredit(long credit) {
+        this.credit = credit;
+    }
+
+    @Override
+    public String toString() {
+        return "UserEntity{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", credit=" + credit +
+                '}';
+    }
 }
