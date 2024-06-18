d05818633bb5499535812a89cb34f9a17a7ae468
dnielpy <reynaldo.dquesada@estudiantes.matcom.uh.cu>
2024-06-18 14:24:00 -0400

    05 - Inventory Admin - ProductEntity, AdminEntity - Created

---
+++ 05 - Inventary Admin (CRUD, Spring Security, Relational db)/src/main/java/com/example/demo/entitys/AdminEntity.java
@@ -0,0 +1,2 @@
+package com.example.demo.entitys;public class AdminEntity {
+}
---
+++ 05 - Inventary Admin (CRUD, Spring Security, Relational db)/src/main/java/com/example/demo/entitys/ProductEntity.java
@@ -0,0 +1,68 @@
+package com.example.demo.entitys;
+
+import jakarta.persistence.*;
+
+@Entity
+public class ProductsEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "price")
+    private long price;
+    @Column(name = "stock")
+    private long stock;
+
+    public ProductsEntity(long id, String name, long price, long stock) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public ProductsEntity() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+
+    public long getStock() {
+        return stock;
+    }
+
+    public void setStock(long stock) {
+        this.stock = stock;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductsEntity{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", stock=" + stock +
+                '}';
+    }
+}
