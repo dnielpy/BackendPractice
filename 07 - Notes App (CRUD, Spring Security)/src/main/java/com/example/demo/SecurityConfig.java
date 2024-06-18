90842cbc243d5241c2331aac5f12d32c95536222
dnielpy <reynaldo.dquesada@estudiantes.matcom.uh.cu>
2024-06-17 23:52:59 -0400

    07 - PROYECTO TERMINADO

--- 07 - Notes App (CRUD, Spring Security)/src/main/java/com/example/demo/SecurityConfig.java
+++ 07 - Notes App (CRUD, Spring Security)/src/main/java/com/example/demo/SecurityConfig.java
@@ -15,7 +15,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
 import java.util.ArrayList;
 import java.util.List;
 
