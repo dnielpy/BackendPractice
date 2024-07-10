59a88228d93ad56cf9e6d421d9cdbf9559e04f31
dnielpy <reynaldo.dquesada@estudiantes.matcom.uh.cu>
2024-07-10 14:18:51 -0400

    05 - Arregladas todas las plantillas para que ejecuten las rutas desde el server

--- 05 - Product Store (JPA, Spring Security, JWT)/src/main/java/com/example/demo/Auth/HomeController.java
+++ 05 - Product Store (JPA, Spring Security, JWT)/src/main/java/com/example/demo/Auth/HomeController.java
@@ -9,4 +9,60 @@ public class HomeController {
     public String index() {
         return "index";
     }
+
+    @RequestMapping("/change-password")
+    public String changePassword() {
+        return "change-password";
+    }
+
+    @RequestMapping("/product")
+    public String product() {
+        return "product";
+    }
+
+    @RequestMapping("/account")
+    public String account() {
+        return "account";
+    }
+
+    @RequestMapping("/orders")
+    public String orders() {
+        return "orders";
+    }
+
+    @RequestMapping("/shop")
+    public String shop() {
+        return "shop";
+    }
+
+    @RequestMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+    @RequestMapping("/contact")
+    public String contact() {
+        return "contact";
+    }
+
+    @RequestMapping("/refund")
+    public String refund() {
+        return "refund";
+    }
+
+    @RequestMapping("/terms")
+    public String terms() {
+        return "terms";
+    }
+
+    @RequestMapping("/cart")
+    public String cart() {
+        return "cart";
+    }
+
+    @RequestMapping("/checkout")
+    public String checkout() {
+        return "checkout";
+    }
+
 }
