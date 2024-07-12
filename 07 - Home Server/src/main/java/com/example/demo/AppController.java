8f0dece889f737b28a457d08e813123e7d0be729
dnielpy <reynaldo.dquesada@estudiantes.matcom.uh.cu>
2024-07-12 15:12:58 -0400

    07 - Ya manda videos, pero el enlace de descarga esta erroneo

--- 07 - Home Server/src/main/java/com/example/demo/AppController.java
+++ 07 - Home Server/src/main/java/com/example/demo/AppController.java
@@ -1,21 +1,26 @@
 package com.example.demo;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-import java.net.InetAddress;
-import java.net.NetworkInterface;
 import java.net.SocketException;
-import java.net.UnknownHostException;
-import java.util.Enumeration;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 @Controller
 public class AppController {
+
+    Service service = new Service();
+
     @GetMapping("/path")
     public String path(Model model) {
         try {
-            String ipAddress = getLocalIpAddress();
+            String ipAddress = Service.getLocalIpAddress();
             if (ipAddress != null) {
                 model.addAttribute("serverIp", ipAddress);
             }
@@ -25,23 +30,27 @@ public class AppController {
         return "path";
     }
 
-    private String getLocalIpAddress() throws SocketException {
-        Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
-        while (networkInterfaces.hasMoreElements()) {
-            NetworkInterface networkInterface = networkInterfaces.nextElement();
-            Enumeration<InetAddress> inetAddresses = networkInterface.getInetAddresses();
-            while (inetAddresses.hasMoreElements()) {
-                InetAddress inetAddress = inetAddresses.nextElement();
-                if (!inetAddress.isLoopbackAddress() && inetAddress.isSiteLocalAddress()) {
-                    return inetAddress.getHostAddress();
-                }
-            }
-        }
-        return null;
+    @PostMapping("/path")
+    public ResponseEntity<String> setPath(@RequestBody String path) {
+        path = URLDecoder.decode(path, StandardCharsets.UTF_8);
+        path = path.replace("\\", "/");
+        path = path.replace("path=", "");
+        service.setPath(path);
+        return ResponseEntity.ok().body("Path set successfully");
     }
 
     @GetMapping("/")
     public String home() {
+        if (service.getPath() == null) {
+            return "path";
+        }
         return "index";
+    }
+
+    @GetMapping("/videos")
+    public String getVideos(Model model) {
+        model.addAttribute("videos", service.getVideoFiles());
+        model.addAttribute("path", service.getPath());
+        return "videos";
     }
 }
--- 07 - Home Server/src/main/java/com/example/demo/Service.java
+++ 07 - Home Server/src/main/java/com/example/demo/Service.java
@@ -1,4 +1,62 @@
 package com.example.demo;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+
 public class Service {
+
+    String path;
+
+    public Service(String path) {
+        this.path = path;
+    }
+
+    public Service() {
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    //Get IP
+    static String getLocalIpAddress() throws SocketException {
+        Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+        while (networkInterfaces.hasMoreElements()) {
+            NetworkInterface networkInterface = networkInterfaces.nextElement();
+            Enumeration<InetAddress> inetAddresses = networkInterface.getInetAddresses();
+            while (inetAddresses.hasMoreElements()) {
+                InetAddress inetAddress = inetAddresses.nextElement();
+                if (!inetAddress.isLoopbackAddress() && inetAddress.isSiteLocalAddress()) {
+                    return inetAddress.getHostAddress();
+                }
+            }
+        }
+        return null;
+    }
+
+    //Get Videos
+    public List<String> getVideoFiles() {
+        List<String> videoFiles = new ArrayList<>();
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(Paths.get(path), "*.{ts,mp4,mkv,avi}")) {
+            for (Path path : stream) {
+                videoFiles.add(path.getFileName().toString());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading video folder", e);
+        }
+        return videoFiles;
+    }
 }
