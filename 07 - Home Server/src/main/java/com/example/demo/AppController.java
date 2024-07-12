befec384451174d8fea63fc91653a5cb9fffc943
dnielpy <reynaldo.dquesada@estudiantes.matcom.uh.cu>
2024-07-11 20:48:37 -0400

    07 - El ip te lo da la web ahora

--- 07 - Home Server/src/main/java/com/example/demo/AppController.java
+++ 07 - Home Server/src/main/java/com/example/demo/AppController.java
@@ -1,13 +1,43 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
 
 @Controller
 public class AppController {
     @GetMapping("/path")
-    public String path() {
+    public String path(Model model) {
+        try {
+            String ipAddress = getLocalIpAddress();
+            if (ipAddress != null) {
+                model.addAttribute("serverIp", ipAddress);
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
         return "path";
+    }
+
+    private String getLocalIpAddress() throws SocketException {
+        Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+        while (networkInterfaces.hasMoreElements()) {
+            NetworkInterface networkInterface = networkInterfaces.nextElement();
+            Enumeration<InetAddress> inetAddresses = networkInterface.getInetAddresses();
+            while (inetAddresses.hasMoreElements()) {
+                InetAddress inetAddress = inetAddresses.nextElement();
+                if (!inetAddress.isLoopbackAddress() && inetAddress.isSiteLocalAddress()) {
+                    return inetAddress.getHostAddress();
+                }
+            }
+        }
+        return null;
     }
 
     @GetMapping("/")
